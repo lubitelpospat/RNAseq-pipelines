f3db7b752a4f8bfc0f05e568c58a9c0dd6544a6a
Alex <sizykh.ad@phystech.edu>
2021-02-12 13:58:10 +0300

    No multiCPU

--- run_batch.sh
+++ run_batch.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --ntasks=8
-#SBATCH --cpus-per-task=4
 #SBATCH --partition=RT
-#SBATCH --job-name=example
+#SBATCH --job-name=download_
 #SBATCH --comment="Скачивание ридов, выравнивание и подсчет каунтов"
  cd ~/RNAseq-pipelines/
  FILE="Accessions.txt"
-if [! -f "$FILE"]; then 
+if [ ! -f "$FILE" ]; then 
     echo "File Accessions.txt doesn\`t exist, exiting"
     exit 1
 fi
@@ -14,8 +13,8 @@ fi
 while IFS= read line
 do
         # display $line or do something with $line
-	echo "$line"
-    srun -o "/totalLogs/$line.out"./pipe1.sh $line
+	touch "totalLogs/$line.out" 
+    srun  -N1 -n1 -c1 --exclusive -o "totalLogs/$line.out" ./pipe1.sh $line
 done <"$FILE"
 
 
