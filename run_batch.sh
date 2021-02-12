2afd047043438adcb0accfbe50674963d3ea653c
Alex <sizykh.ad@phystech.edu>
2021-02-12 14:00:10 +0300

    Added &

--- run_batch.sh
+++ run_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=8
 #SBATCH --partition=RT
-#SBATCH --job-name=download_
+#SBATCH --job-name=download_align_count
 #SBATCH --comment="Скачивание ридов, выравнивание и подсчет каунтов"
  cd ~/RNAseq-pipelines/
  FILE="Accessions.txt"
@@ -14,7 +14,7 @@ while IFS= read line
 do
         # display $line or do something with $line
 	touch "totalLogs/$line.out" 
-    srun  -N1 -n1 -c1 --exclusive -o "totalLogs/$line.out" ./pipe1.sh $line
+    srun  -N1 -n1 -c1 --exclusive -o "totalLogs/$line.out" ./pipe1.sh $line &
 done <"$FILE"
 
 
